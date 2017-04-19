88af977d21da4ea4c2b4fdc86b9b7955f42569fd
Valérian Girard <valerian.girard@cfast-web.fr>
2017-04-19 10:29:38 +0200

    Add Annotation for model validation

---
+++ src/GST.Library.Data/Model/DataAnnotations/InListAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+
+namespace GST.Library.Data.Model.DataAnnotations
+{
+    /// <summary>
+    /// Validate that the data is in the list
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class InListAttribute : ValidationAttribute
+    {
+        private IEnumerable<object> expectedList;
+
+        /// <summary>
+        /// List of things
+        /// </summary>
+        /// <param name="expectedList"></param>
+        public InListAttribute(IEnumerable<object> expectedList) : base(() => "The field {0} is not an allowed value")
+        {
+            this.expectedList = expectedList;
+        }
+
+        /// <summary>
+        /// Override of <see cref="ValidationAttribute.IsValid(object)"/>
+        /// </summary>
+        /// <remarks>This method returns <c>true</c> if the <paramref name="value"/> is null.
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+
+            return this.expectedList.Any(el => el == value);
+        }
+
+        /// <summary>
+        /// Override of <see cref="ValidationAttribute.FormatErrorMessage"/>
+        /// </summary>
+        /// <param name="name">The name to include in the formatted string</param>
+        /// <returns>A localized string to describe the maximum acceptable length</returns>
+        /// <exception cref="InvalidOperationException"> is thrown if the current attribute is ill-formed.</exception>
+        public override string FormatErrorMessage(string name)
+        {
+            return String.Format(CultureInfo.CurrentCulture, this.ErrorMessageString, name);
+        }
+    }
+}
---
+++ test/GST.Library.Data.Tests/Model/DataAnnotations/InListTest.cs
@@ -0,0 +1,39 @@
+﻿using GST.Library.Data.Model.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GST.Library.Data.Tests.Model.DataAnnotations
+{
+    public class InListTest
+    {
+        [Fact]
+        public void mustValidData()
+        {
+            InListAttribute il = new InListAttribute(new[] {
+                "value 1",
+                "value 2",
+                "value 3"
+            });
+
+            Assert.True(il.IsValid("value 1"));
+            Assert.True(il.IsValid("value 2"));
+            Assert.True(il.IsValid("value 3"));
+
+            Assert.False(il.IsValid("value 4"));
+        }
+
+        public void mustFormatErrorMessage()
+        {
+            InListAttribute il = new InListAttribute(new[] {
+                "value 1",
+                "value 2",
+                "value 3"
+            });
+
+            Assert.Equal("The field aFieldName is not an allowed value", il.FormatErrorMessage("aFieldName"));
+        }
+    }
+}
