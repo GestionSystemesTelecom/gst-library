ec78b76f93ed9f05f0e13fa7c9b683cab0ca813e
waldo2188 <waldo2188@gmail.com>
2018-06-19 10:03:51 +0200

    Simplify service creation

---
+++ src/GST.Library.TimeZone/DependencyInjection/GSTLibraryTimeZoneServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using GST.Library.TimeZone.Services;
+using GST.Library.TimeZone.Services.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GST.Library.TimeZone.DependencyInjection
+{
+    /// <summary>
+    /// DI extension methods for adding TimeZone service
+    /// </summary>
+    public static class GSTLibraryTimeZoneServiceCollectionExtensions
+    {
+
+        /// <summary>
+        /// Add TimeZone service
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddTimeZoneService(this IServiceCollection services)
+        {
+            services.AddScoped<ITimeZoneHelper, TimeZoneHelper>();
+        }       
+    }
+}
